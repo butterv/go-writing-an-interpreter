0247f5830cd55a8bd84191f5fb0f1eb814edfd86
istsh <github3104xniiapm@gmail.com>
2019-10-06 02:24:45 +0900

    Finish 2.6.5

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -128,3 +128,11 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	return stmt
 }
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
