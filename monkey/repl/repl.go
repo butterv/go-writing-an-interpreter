74b3119a40e7ec1dedc24666c99208a7973554a9
istsh <github3104xniiapm@gmail.com>
2019-10-15 00:25:50 +0900

    Finish 3.5.2

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/istsh/go-writing-an-interpreter/monkey/evaluator"
 	"github.com/istsh/go-writing-an-interpreter/monkey/lexer"
 	"github.com/istsh/go-writing-an-interpreter/monkey/parser"
 )
@@ -28,10 +29,14 @@ func Start(in io.Reader, out io.Writer) {
 		program := p.ParseProgram()
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
+			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
