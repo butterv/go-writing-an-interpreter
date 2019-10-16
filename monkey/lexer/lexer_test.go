2700c4bbc9dbe3586d5ea05909e26b1bcfb61d18
istsh <github3104xniiapm@gmail.com>
2019-10-16 23:44:09 +0900

    WIP: Refactoring lexer test

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -112,10 +112,10 @@ if (5 < 10) {
 		{token.EOF, ""},
 	}
 
-	lexer := New(input)
+	l := New(input)
 
 	for i, tt := range tests {
-		tok := lexer.NextToken()
+		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
